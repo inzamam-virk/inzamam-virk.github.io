a9b9c0b13f70d63b8ef756de21d5eb36ba6e82e4
Inzamam_virk084 <inzamam.virk@dhives.com>
2023-09-24 21:39:14 +0500

    Updated footer and articles

--- src/data/articles.js
+++ src/data/articles.js
@@ -3,6 +3,45 @@ import React from "react";
 function article_1() {
 	return {
 		date: "23 Sep 2023",
+		title: "Deep Dive into React",
+		description:
+			"React course from beginner to advance, exploring advance concepts",
+		keywords: [
+			"Deep Dive into React",
+			"Inzamam",
+			"InzamamVirk",
+			"Inzamam Virk",
+		],
+		style: `
+				.article-content {
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+				}
+
+				.randImage {
+					align-self: center;
+					outline: 2px solid red;
+				}
+				`,
+		body: (
+			<React.Fragment>
+				<div className="article-content">
+					<div className="paragraph">Coming soon! Be connected!</div>
+					{/* <img
+						src="https://picsum.photos/200/300"
+						alt="random"
+						className="randImage"
+					/> */}
+				</div>
+			</React.Fragment>
+		),
+	};
+}
+
+function article_2() {
+	return {
+		date: "23 Sep 2023",
 		title: "Why to learn javascript",
 		description:
 			"JavaScript’s popularity stems from a combination of its inherent design characteristics, capabilities, and the ecosystem that has evolved around it.",
@@ -39,7 +78,7 @@ function article_1() {
 	};
 }
 
-function article_2() {
+function article_3() {
 	return {
 		date: "23 Sep 2023",
 		title: "6 Stages of Debugging!",
@@ -67,6 +106,6 @@ function article_2() {
 	};
 }
 
-const myArticles = [article_1, article_2];
+const myArticles = [article_1, article_2, article_3];
 
 export default myArticles;
